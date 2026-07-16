828b7eee27e01e17e14fb7d7ffcabc3af5ce15cd
swapnachari-marjuk <marjukmujaddedi@gmail.com>
2026-07-16 21:09:04 +0600

    first variable diclaration succeed.

---
+++ First_P/fvd.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int name();
+
+int main()
+{
+    int num = 2;
+    printf("%d\n", num);
+    name();
+    return 0;
+}
+
+int name () {
+    char name_My[] = "marjuk";
+    printf("%s\n", name_My);
+    return 0;
+}
+
+
+
